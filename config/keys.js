a08fb21676ca27deeb3593ef50ddf709c6fb4c22
juanmurillog <jjmurillob@ut.edu.co>
2020-06-21 01:01:34 -0500

    Updates

--- config/keys.js
+++
@@ -1,8 +0,0 @@
-
-if (process.env.NODE_ENV === 'production') {
-    // We are in production. Return the prod set of keys.
-    module.exports = require('./prod');
-  } else {
-    // We are in development. Return the dev set of keys.
-    module.exports = require('./dev');
-  }
---
+++ precache-manifest.84cd2f6eed83c1211b62830b4003ca9d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f615e0de670c11af37f19d13844885d0",
+    "url": "/juanmurillog/exequiales-monte-santo.git/index.html"
+  },
+  {
+    "revision": "9daf1b777de30de994a4",
+    "url": "/juanmurillog/exequiales-monte-santo.git/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6f0db178349d389c81f4",
+    "url": "/juanmurillog/exequiales-monte-santo.git/static/css/main.57702e2b.chunk.css"
+  },
+  {
+    "revision": "9daf1b777de30de994a4",
+    "url": "/juanmurillog/exequiales-monte-santo.git/static/js/2.138ee003.chunk.js"
+  },
+  {
+    "revision": "33c02c42ab7314be00ddaa44dbd96c39",
+    "url": "/juanmurillog/exequiales-monte-santo.git/static/js/2.138ee003.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f0db178349d389c81f4",
+    "url": "/juanmurillog/exequiales-monte-santo.git/static/js/main.d3bc2d03.chunk.js"
+  },
+  {
+    "revision": "19317363af672827a26c",
+    "url": "/juanmurillog/exequiales-monte-santo.git/static/js/runtime-main.8d209fd1.js"
+  }
+]);
