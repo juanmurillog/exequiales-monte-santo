af1a44f3b54d2d3539faa5d36db1adb78d59ebe2
juanmurillog <jjmurillob@ut.edu.co>
2020-06-20 23:19:23 -0500

    configuracion de package

--- src/Pages/quienesSomos.js
+++ src/Pages/quienesSomos.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '../Components/Home/header'
 import Footer from '../Components/Home/footer'
 import Portada from '../Components/QuienesSomos/portada'
+import { Row, Col, Container } from "react-bootstrap";
   function QuienesSomos(){
 
     return(
@@ -10,7 +11,13 @@ import Portada from '../Components/QuienesSomos/portada'
           <section>
             <Portada/>
           </section>
-          <Footer/>
+          <Row className="container-row d-flex align-items-center" style={{backgroundColor: "var(--pdf2)" , color: "white"}}>
+          <Col className="container-col" md="8" lg="8">
+            <p className="text-center text-md-left grey-text">
+              &copy; {new Date().getFullYear()} Copyright:{" "}
+            </p>
+          </Col>       
+        </Row>
           
           </div>
           
